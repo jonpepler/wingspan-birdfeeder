6b7596d64d724d7c98db06c34553ea375d92670d
Jon Pepler <jonathan.pepler@gmail.com>
2022-06-30 17:27:30 +0100

    fix: unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { version } from '../package.json'
-
 import { Birdfeeder } from './components/Birdfeeder'
 import { DiceDisplay } from './components/DiceDisplay'
 import { useBirdfeeder } from './hooks/useBirdfeeder'
